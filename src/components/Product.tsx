6620d9ebe452daafe248450ca9f24f19b7c578d4
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-02-04 21:39:02 +0300

    ErrorBoundary && axios

---
+++ src/components/Product.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { IProduct } from "../models";
+
+interface ProductProps {
+  product: IProduct;
+}
+
+const Product = (props: ProductProps) => {
+  const { product } = props;
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(prev => !prev);
+  };
+
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img src={product.image} className="w-1/6" alt={product.title} />
+      <p>{product.title}</p>
+      <p className="font-bold mb-10">{product.price}</p>
+      <p>{product.rating.rate}</p>
+      <p>{product.rating.count}</p>
+      <button className={`border-${open ? 'red-800' : 'gray-100'} border-4 p-3`} onClick={handleOpen}>
+        {open ? 'hide' : 'show'} details
+      </button>
+      {open && <p className="italic pt-3">{product.description}</p>}
+    </div>
+  );
+};
+
+export default Product;
