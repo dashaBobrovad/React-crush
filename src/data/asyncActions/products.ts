607820c94d8198c95c4ba72dd7f672e934ead821
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-05-04 22:53:58 +0300

    mock data & format scss & fx preloader

--- src/data/asyncActions/products.ts
+++ src/data/asyncActions/products.ts
@@ -2,16 +2,18 @@ import axios, { AxiosError } from "axios";
 import { Dispatch } from "redux";
 import { ProductsAction } from "../../types/productsReducerTypes";
 import { getProductsAction } from "../reducers/productsReducer";
+import dataMocks from "../mocks";
 
 const fetchProducts = () => async (dispatch: Dispatch<ProductsAction>) => {
   axios
-    .get("https://fakestoreapi.com/products")
+    .get("http://localhost:3000/db/data.json")
     .then((response) => {
       dispatch(getProductsAction(response.data));
     })
     .catch((e: unknown) => {
       const error = e as AxiosError;
       console.error(new Error("error", error));
+      dispatch(getProductsAction(dataMocks));
     });
 };
 
