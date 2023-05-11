7f3a7321738d1f85f12ff7cbcc2f7d92d0919d88
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-05-11 22:20:38 +0300

    format reducer

--- src/data/reducers/productsReducer.ts
+++ src/data/reducers/productsReducer.ts
@@ -8,8 +8,8 @@ import {
 const initialState: IProductsState = {
   products: [],
   basket: {
-    list: [], 
-    totalPrice: 0, 
+    list: [],
+    totalPrice: 0,
     totalCount: 0
   },
 };
@@ -19,72 +19,80 @@ const productsReducer = (
   action: ProductsAction
 ): IProductsState => {
 
-  switch (action.type) {
-    case ProductsActionTypes.GET_PRODUCTS:
+switch (action.type) {
+
+  // ---------- GET_PRODUCTS ----------
+  case ProductsActionTypes.GET_PRODUCTS:
+    return {
+      ...state,
+      products: action.payload.map((item) => Object.assign(item, { price: Math.round(item.price) }))
+    };
+  
+  // ---------- ADD_PRODUCTS_TO_BASKET ----------
+  case ProductsActionTypes.ADD_PRODUCTS_TO_BASKET:
+    if (state.basket.list.find((product: IProductBasket) => product.id === action.payload.id)) {
       return {
         ...state,
-        products: action.payload.map((item) => Object.assign(item, {price: Math.round(item.price)}))
-      };
-    case ProductsActionTypes.ADD_PRODUCTS_TO_BASKET:
-      if (state.basket.list.find((product: IProductBasket) => product.id === action.payload.id)) {
-        return {
-          ...state,
-          basket: 
-            {...state.basket, 
-            list: state.basket.list.map((item : IProductBasket) => item.id === action.payload.id
-            ? {
-              ...item,
-              qty: item.qty ? item.qty  + 1 : 2,
-            }
-            : item,
+        basket:
+          {
+            ...state.basket,
+            list: state.basket.list.map((item: IProductBasket) => item.id === action.payload.id
+              ? {
+                ...item,
+                qty: item.qty ? item.qty + 1 : 2,
+              }
+              : item,
             ),
-          totalPrice: state.basket.totalPrice + action.payload.price,
-          totalCount: state.basket.totalCount + 1,}
-        };
-      }
-    
-      return {
-        ...state,
-        basket: 
-          {...state.basket,
-          
-            list: [...state.basket.list, action.payload], 
             totalPrice: state.basket.totalPrice + action.payload.price,
             totalCount: state.basket.totalCount + 1,
           }
-        ,
       };
-    case ProductsActionTypes.REMOVE_PRODUCTS_FROM_BASKET: 
+    }
+
+    return {
+      ...state,
+      basket:
+      {
+        ...state.basket,
+        list: [...state.basket.list, action.payload],
+        totalPrice: state.basket.totalPrice + action.payload.price,
+        totalCount: state.basket.totalCount + 1,
+      },
+    };
+    
+    // ---------- REMOVE_PRODUCTS_FROM_BASKET ----------
+    case ProductsActionTypes.REMOVE_PRODUCTS_FROM_BASKET:
       if (state.basket.list.find((product: IProductBasket) => product.id === action.payload.id && product && product.qty && product.qty > 1)) {
         return {
           ...state,
-          basket: 
-            {...state.basket, 
-            list: state.basket.list.map((item : IProductBasket) => item.id === action.payload.id 
-            ? {
-              ...item,
-              qty: item.qty && item.qty - 1,
-            }
-            : item,
-            ),
-            totalPrice: state.basket.totalPrice - action.payload.price,
-            totalCount: state.basket.totalCount - 1,
-        }
-        };
-      } 
-
-
-      console.log(`qty ${action.payload.qty}`);
-
-        return {...state,
-          basket: 
-            {...state.basket, 
-              list: state.basket.list.filter(item => item.id !== action.payload.id 
-                ),
+          basket:
+            {
+              ...state.basket,
+              list: state.basket.list.map((item: IProductBasket) => item.id === action.payload.id
+                ? {
+                  ...item,
+                  qty: item.qty && item.qty - 1,
+                }
+                : item,
+              ),
               totalPrice: state.basket.totalPrice - action.payload.price,
               totalCount: state.basket.totalCount - 1,
             }
-      };
+        };
+      }
+
+      return {
+        ...state,
+        basket:
+          {
+            ...state.basket,
+            list: state.basket.list.filter(item => item.id !== action.payload.id),
+            totalPrice: state.basket.totalPrice - action.payload.price,
+            totalCount: state.basket.totalCount - 1,
+          }
+        };
+    
+    // ---------- default ----------
     default:
       return state;
   }
@@ -105,4 +113,9 @@ const removeProductsFromBasketAction = (payload: IProduct): ProductsAction => ({
   payload,
 });
 
-export { getProductsAction, addProductsToBasketAction, productsReducer , removeProductsFromBasketAction};
+export { 
+  getProductsAction, 
+  addProductsToBasketAction, 
+  productsReducer, 
+  removeProductsFromBasketAction 
+};
