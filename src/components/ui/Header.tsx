af60b4d15840678e1246ca933321f1cc35a85be7
dashaBobrovad <dbobrova.dasha@gmail.com>
2023-05-12 13:28:17 +0300

    counter on basket

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,10 +1,15 @@
 import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { shallowEqual } from "react-redux";
 import s from "../../assets/scss/components/ui/Header.module.scss";
 import Icon from "./Icon";
 import Navigation from "./Navigation";
+import Counter, { CounterSizeTypes } from "./Counter";
+import useTypedSelector from "../../data/hooks/useTypedSelector";
 
 function Header() {
+  const totalCount = useTypedSelector((state) => state.products.basket.totalCount, shallowEqual);
+
   return (
     <div className={s.header}>
       <div className={`${s.content} column`}>
@@ -12,6 +17,7 @@ function Header() {
         <Link to="/basket">
           {" "}
           <div className={s.basket}>
+            <Counter count={totalCount} size={CounterSizeTypes.SM}/>
             <Icon icon={faBasketShopping} />
           </div>
         </Link>
